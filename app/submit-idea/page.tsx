579e93b433c871869db54b8eb69ab609629958cf
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2026-01-23 23:24:02 +0530

    feat: added submit-idea route and notion link for prelims

---
+++ app/submit-idea/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function SubmitIdeaPage() {
+	redirect("https://glittery-mallow-ca5.notion.site/DoubleSlash-4-0-1d042d6d80c2809ea10ff6055de02e9f");
+}
