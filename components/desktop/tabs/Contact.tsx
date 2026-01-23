761ea6bbebc000a385672928040bc9888a0fa569
sarinsanyal <sarinsanyal2005@gmail.com>
2026-01-24 01:01:45 +0530

    Added mail

--- components/desktop/tabs/Contact.tsx
+++ components/desktop/tabs/Contact.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <p>
+      Contact
+      </p>
+      <a href = "mailto:doubleslash.ieeejusb@gmail.com" className="text-blue-700 underline">
+        Send Email
+      </a>
+    </div>
   )
 }
 
